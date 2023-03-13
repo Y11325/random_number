b3198de6e324c6a810e25c414e402a860e6301ab
Amy <88516512+Y11325@users.noreply.github.com>
2023-03-13 15:14:46 -0700

    Update task2.py

---
+++ task2.py
@@ -0,0 +1,40 @@
+#! python3
+
+# SD Computing Studies Assignment
+'''
+##### Task 2
+Coin Toss
+Ask the player to choose heads or tails.
+Toss a coin to determine what the coin will be, and then tell the player if they were correct.
+(2 points)
+'''
+print("\n\n<<<---Heads or Tails--->>>")
+import random
+yes = 100
+
+while yes == 100:
+    coin = random.randint(0,1)
+    print("\nCoin tossed...")
+    print(100*"*")
+
+    guess = input("\nGuess heads or tails: ")
+    if guess == "heads":
+        guess = 0
+    elif guess == "tails":
+        guess = 1
+
+    if guess != coin:
+        print("\nWrong!\n\n")
+        again = input("Play again (enter yes or no):")
+        if again == "yes":
+            continue
+        elif again == "no":
+            break
+    elif guess == coin:
+        print("\nCorrect!\n\n")
+        again = input("Play again (enter yes or no):")
+        if again == "yes":
+            continue
+        elif again == "no":
+            break
+print("\n---Thanks for playing---\n\n")
